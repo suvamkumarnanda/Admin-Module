342131f02eedc98a81f7caebaa79ebb25f537dae
Suvam Kumar Nanda <68044124+suvamkumarnanda@users.noreply.github.com>
2023-06-01 22:59:56 +0530

    Update Train.java
    Add No Args Constructor

--- Admin-Module/src/main/java/com/cap/dto/Train.java
+++ Admin-Module/src/main/java/com/cap/dto/Train.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Train {
-	
+	public Train(){}
 	
 	private Integer  trainNum;
 	public Integer getTrainNum() {
